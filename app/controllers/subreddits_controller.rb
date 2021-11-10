f92d76992c4ecedca10c2dad5e16d756e585afe3
“esseme” <esseme@gmail.com>
2021-11-10 15:22:18 +0300

    Add subreddit model

--- app/controllers/subreddits_controller.rb
+++ app/controllers/subreddits_controller.rb
@@ -1,5 +1,14 @@
 class SubredditsController < ApplicationController
+  before_action :set_subreddit, only: :show 
   def show
-    
+
   end
+
+  private
+
+  def set_subreddit
+    @subreddit = Subreddit.find(params[:id])
+  end
+
+
 end
